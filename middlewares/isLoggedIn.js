6e9cfbc16f36c9543b2b5b67fdb3b067874d4090
Sumith Kumar Saini <sumithsaini01@gmail.com>
2024-08-05 12:10:47 +0530

    Starting from cloning Pinterest

--- middlewares/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-const isLoggedIn = async function (req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/account/login"); // Redirect to login or another appropriate page
-};
-
-module.exports = isLoggedIn;
--- utils/userspliter.js
+++
@@ -1,6 +0,0 @@
-function nameSplitter(username = '') {
-    let name = username.split(" ");
-    return name.join("_");
-}
-
-module.exports = nameSplitter;
